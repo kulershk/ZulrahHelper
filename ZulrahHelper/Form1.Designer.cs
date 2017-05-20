abae4794ac6ede87be91299d983b20f677f25a5c
kulershk <kulershk@gmail.com>
2017-05-20 17:26:17 +0300

    Add files via upload

---
+++ ZulrahHelper/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ZulrahHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZulrahHelper.Properties.Resources.Base;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 109);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Previous";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ZulrahHelper.Properties.Resources.protection;
+            this.pictureBox2.Location = new System.Drawing.Point(147, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(108, 108);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(283, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(364, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "2";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lime;
+            this.button6.Location = new System.Drawing.Point(283, 36);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "3";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Aqua;
+            this.button7.Location = new System.Drawing.Point(364, 36);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "4";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 142);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(463, 181);
+            this.MinimumSize = new System.Drawing.Size(463, 181);
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "Zulrah helper by kulers";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ZulrahHelper/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web.Script.Serialization;
+
+namespace ZulrahHelper
+{
+    public partial class Form1 : Form
+    {
+        //{Location,Phase}
+        int cWave = 0;
+        Rot[] rot;
+        ZulrahLocations[] zLocation = new ZulrahLocations[4];
+        PlayerLocation[] pLocation = new PlayerLocation[5];
+
+        Bitmap DrawArea;
+        int rotation = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            pictureBox1.Image = DrawArea;
+
+            //z locations
+            zLocation[0] = new ZulrahLocations { x = 64, y = 64 + 16 };
+            zLocation[1] = new ZulrahLocations { x = 10, y = 64 - 16 };
+            zLocation[2] = new ZulrahLocations { x = 64, y = 10 };
+            zLocation[3] = new ZulrahLocations { x = 128 - 10, y = 64 - 16 };
+            //p location
+            pLocation[0] = new PlayerLocation { x = 34, y = 95 };
+            pLocation[1] = new PlayerLocation { x = 34, y = 60 };
+            pLocation[2] = new PlayerLocation { x = 64, y = 33 };
+            pLocation[3] = new PlayerLocation { x = 94, y = 60 };
+            pLocation[4] = new PlayerLocation { x = 94, y = 95 };
+
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot1.txt"));
+
+            UpdatePic(pictureBox1);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Application.Exit();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot1.txt"));
+
+            UpdatePic(pictureBox1);
+
+        }
+
+        private Image DrawPhase(int mage)
+        {
+            Bitmap cc = new Bitmap(24, 24);
+            Graphics gg;
+            gg = Graphics.FromImage(cc);
+            gg.DrawImage(Properties.Resources.Phases, -24 * mage, 0, 72, 24);
+            return (Image)cc;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cWave++;
+            if (cWave >= rot.Length) cWave = rot.Length - 1;
+            Console.WriteLine("Wave: " + cWave);
+            UpdatePic(pictureBox1);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cWave--;
+            if (cWave <= 0) cWave = 0;
+            Console.WriteLine("Wave: " + cWave);
+            UpdatePic(pictureBox1);
+        }
+
+        private void UpdatePic(PictureBox pp)
+        {
+            if (cWave >= rot.Length) cWave = rot.Length - 1;
+
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+            g.DrawImage(Properties.Resources.Base, 0, 0, 128, 128);
+
+            Pen mypen = new Pen(Brushes.Gray);
+            //Draw circles
+            //Console.WriteLine(phases[rotation, cWave]);
+            g.DrawImage(DrawPhase(rot[cWave].phase), zLocation[rot[cWave].location].x - 12, zLocation[rot[cWave].location].y - 12);
+
+            SolidBrush myBrush = new SolidBrush(Color.Purple);
+            g.FillEllipse(myBrush, new Rectangle(pLocation[rot[cWave].pl].x-5, pLocation[rot[cWave].pl].y-5, 10, 10));
+
+            updateProtection(rot[cWave].pray);
+
+
+
+            mypen = new Pen(Brushes.Black);
+
+            //g.DrawLine(mypen, 0, 0, 128, 128);
+
+            pp.Image = DrawArea;
+            label1.Text = "Rotation: " + rotation + " Wave: " + (cWave + 1)+"/"+ rot.Length;
+        }
+
+        private void updateProtection(int pray)
+        {
+            Graphics g;
+            Bitmap DrawArea2 = new Bitmap(pictureBox2.Size.Width, pictureBox2.Size.Height);
+            g = Graphics.FromImage(DrawArea2);
+            g.DrawImage(Properties.Resources.protection, -108 * pray, 0, 108 * 5, 108);
+
+            pictureBox2.Image = DrawArea2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot1.txt"));
+            rotation = 1;
+
+            UpdatePic(pictureBox1);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot2.txt"));
+            rotation = 2;
+
+            UpdatePic(pictureBox1);
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            cWave = 0;
+            UpdatePic(pictureBox1);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot3.txt"));
+            rotation = 3;
+
+            UpdatePic(pictureBox1);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            rot = js.Deserialize<Rot[]>(File.ReadAllText(@"rot4.txt"));
+            rotation = 4;
+
+            UpdatePic(pictureBox1);
+        }
+    }
+
+    public class Rot
+    {
+        public int phase { get; set; }
+        public int location { get; set; }
+        public int pray { get; set; }
+        public int pl { get; set; }
+    }
+
+    public class ZulrahLocations
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+
+    public class PlayerLocation
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
